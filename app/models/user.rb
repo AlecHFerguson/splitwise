7268d95dbe82b8882f5ba41a38120c94d75abbe7
Alec Ferguson <alec.ferguson@gmail.com>
2014-06-14 15:48:40 -0700

    Encrypting passwords.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ActiveRecord::Base
+
+  before_save :encrypt_password
+  after_save :clear_password
+
   validates :fname, presence: true
   validates :lname, presence: true
   validates :email, presence: true
   validates :password, presence: true
+
+  
+  def encrypt_password
+    salt = BCrypt::Engine.generate_salt
+    self.password = BCrypt::Engine.hash_secret(password, salt)
+  end
+
+  def clear_password
+    self.password = nil
+  end
 end
