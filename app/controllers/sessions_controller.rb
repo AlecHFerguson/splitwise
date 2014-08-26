f075a8e8748132a1c35aa86c798cc4fdd22198b8
Alec Ferguson <alec.ferguson@gmail.com>
2014-08-25 22:41:18 -0700

    Got login form to render.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def login
+    render 'login'
   end
 
   def create
--- test/integration/authentication_pages_test.rb
+++ test/integration/authentication_pages_test.rb
@@ -1,8 +1,23 @@
 require_relative '../test_helper'
 
 class AuthenticationPagesTest < ActionDispatch::IntegrationTest
+  
   test 'get login page' do
     get '/login'
     assert_response :success
+  end
+
+  test 'session created correctly' do
+    post_via_redirect '/create', email: users(:one).email, password: users(:one).password
+    assert_response :success
+    assert_equal '/dashboard', path
+    assert_equal "Welcome, #{users(:one).fname}", flash[:notice]
+  end
+
+  test 'invalid email => NOT logged in' do
+    post '/create', post: {email: 'blah', password: users(:one).password}
+    assert_response :failure
+    assert_equal '/login', path
+    assert_equal 'Invalid password', flash[:notice]
   end
 end
