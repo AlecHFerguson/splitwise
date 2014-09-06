c56227555177e96e5da4b2441226cecda18ff0d8
Alec Ferguson <alec.ferguson@gmail.com>
2014-09-06 08:07:43 -0700

    Added password_confirmation to unit tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,10 @@ require '/home/alec/RailsProjects/splitwise/test/test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     @user = users(:one)
+    @test_fname = 'Abcdefg'
+    @test_lname = 'Hijklmnop'
+    @test_email = 'aaa@bbb.cc'
+    @test_password = 'testing1'
   end
 
   test "should get index" do
@@ -19,7 +23,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should create user" do
     assert_difference('User.count') do
       post :create, user: { email: @user.email, fname: @user.fname, lname: @user.lname, 
-                            password: @user.password, password_confirmation: @user.password }
+                            password: @test_password, password_confirmation: @test_password }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -42,7 +46,9 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, fname: @user.fname, lname: @user.lname, password: @user.password }
+    patch :update, id: @user.id, user: { email: @user.email, fname: @user.fname, 
+              lname: @user.lname, password: @test_password + 'aaa', 
+              password_confirmation: @test_password + 'aaa'  }
     assert_redirected_to user_path(assigns(:user))
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,35 +1,45 @@
 require_relative '../test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  injection_string = "'; Show tables;--"
+  setup do
+    @test_fname = 'Abcdefg'
+    @test_lname = 'Hijklmnop'
+    @test_email = 'aaa@bbb.cc'
+    @test_password = 'testing1'
+    @injection_string = "'; Show tables;--"
+  end
 
   test 'Valid user is saved' do
-    user = User.new({ fname: 'Aaa', lname: 'Bbb', email: 'a@b.c', password: 'test' })
+    user = User.new({ fname: @test_fname, lname: @test_lname, email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password })
     assert user.save
   end
 
   ## :fname tests
   test 'Invalid fname => fails to save' do
-    user = User.new({ fname: 'abcdefg', lname: 'Bbb', email: 'a@b.c', password: 'test'})
+    user = User.new({ fname: 'abcdefg', lname: @test_lname, email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password })
     assert_not user.save
     assert_equal(user.errors.messages, { fname: ["is invalid"] } )
   end
 
   test 'SQL injection fname => fails to save' do
-    user = User.new({ fname: injection_string, lname: 'Bbb', email: 'a@b.c', 
-                      password: 'test'})
+    user = User.new({ fname: @injection_string, lname: @test_lname, email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password })
     assert_not user.save
     assert_equal(user.errors.messages, { fname: ["is invalid"] } )
   end
 
   test 'Blank fname => fails to save' do
-    user = User.new({ fname: '', lname: 'Bbb', email: 'a@b.c', password: 'test'})
+    user = User.new({ fname: '', lname: @test_lname, email: @test_email, password: @test_password,
+                      password_confirmation: @test_password })
     assert_not user.save
     assert_equal(user.errors.messages, { fname: ["can't be blank", 'is invalid'] })
   end
 
   test 'Missing :fname => fails to save' do
-    user = User.new({ lname: 'Bbb', email: 'a@b.c', password: 'test'})
+    user = User.new({ lname: @test_lname, email: @test_email, password: @test_password, 
+                      password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { fname: ["can't be blank", "is invalid"] })
   end
@@ -37,26 +47,29 @@ class UserTest < ActiveSupport::TestCase
 
   ## :lname tests
   test 'Invalid lname => fails to save' do
-    user = User.new({ fname: 'Abcdefg', lname: 'abcdefg', email: 'a@b.c', password: 'test'})
+    user = User.new({ fname: 'Abcdefg', lname: 'abcdefg', email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { lname: ["is invalid"] })
   end
 
   test 'SQL injection lname => fails to save' do
-    user = User.new({ fname: 'Abc', lname: injection_string, email: 'a@b.c', 
-                      password: 'test'})
+    user = User.new({ fname: 'Abc', lname: @injection_string, email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { lname: ["is invalid"] })
   end
 
   test 'Blank lname => fails to save' do
-    user = User.new({ fname: 'Xyz', lname: '', email: 'a@b.c', password: 'test'})
+    user = User.new({ fname: 'Xyz', lname: '', email: @test_email, password: @test_password,
+                      password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { lname: ["can't be blank", 'is invalid'] })
   end
 
   test 'Missing :lname => fails to save' do
-    user = User.new({ fname: 'Bbb', email: 'a@b.c', password: 'test'})
+    user = User.new({ fname: @test_lname, email: @test_email, password: @test_password,
+                      password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { lname: ["can't be blank", "is invalid"] })
   end
@@ -64,43 +77,70 @@ class UserTest < ActiveSupport::TestCase
   ## :email tests
   test 'Invalid email => fails to save' do
     user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: 'arthur#bibimbap.co.kr',
-                      password: 'test'})
+                      password: @test_password, password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { email: ["is invalid"] })
   end
 
   test 'SQL injection email => fails to save' do
-    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', 
-                      email: injection_string, password: 'test'})
+    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: @injection_string, 
+                      password: @test_password, password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { email: ["is invalid"] })
   end
 
   test 'Blank email => fails to save' do
     user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: '',
-                      password: 'test'})
+                      password: @test_password, password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { email: ["can't be blank", "is invalid"] })
   end
 
   test 'Missing email => fails to save' do
-    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', password: 'test'})
+    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', password: @test_password,
+                      password_confirmation: @test_password})
     assert_not user.save
     assert_equal(user.errors.messages, { email: ["can't be blank", "is invalid"] })
   end
 
   ## :password tests
   test 'Short password => fails to save' do
-    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: 'a@b.c', password: 'hi'})
+    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: @test_email, password: 'hi',
+                      password_confirmation: 'hi'})
     assert_not user.save
     assert_equal(user.errors.messages, { password: ['is too short (minimum is 3 characters)'] })
   end
 
   test 'Long password => fails to save' do
-    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: 'a@b.c', 
-                      password: 'LesTroisTetonsLesTroisTetonsLesTroisTetonsLesTroisTetonsLesTroisTetons999abcd'})
+    long_pw = 'LesTroisTetonsLesTroisTetonsLesTroisTetonsLesTroisTetonsLesTroisTetons999abcd'
+    user = User.new({ fname: 'Abcdefg', lname: 'Abcdefg', email: @test_email, 
+                      password: long_pw, password_confirmation: long_pw })
     assert_not user.save
     assert_equal(user.errors.messages, { password: ['is too long (maximum is 50 characters)'] })
   end
 
+  ## :password_confirmation tests
+  test 'Non-matching password_confirmation => fails to save' do
+    user = User.new({ fname: @test_fname, lname: @test_lname, email: @test_email, 
+                      password: @test_password, password_confirmation: @test_password + 'a' })
+    assert_not user.save
+    assert_equal(user.errors.messages, 
+                { password_confirmation: ["doesn't match Password"] } )
+  end
+
+  test 'Missing password_confirmation => fails to save' do
+    user = User.new({ fname: @test_fname, lname: @test_lname, email: @test_email, 
+                      password: @test_password })
+    assert_not user.save
+    assert_equal(user.errors.messages,
+                { password_confirmation: ["can't be blank"] } )
+  end
+
+  test 'Blank password_confirmation => fails to save' do
+    user = User.new({ fname: @test_fname, lname: @test_lname, email: @test_email, 
+                      password: @test_password, password_confirmation: '' })
+    assert_not user.save
+    assert_equal(user.errors.messages, 
+                { password_confirmation: ["doesn't match Password", "can't be blank"] } )
+  end
 end
