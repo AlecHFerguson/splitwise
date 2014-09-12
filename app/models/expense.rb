d8d812fd0031fb2a97b583e8d200e460b3a0f8bd
Alec Ferguson <alec.ferguson@gmail.com>
2014-09-12 07:13:52 -0700

    Added currency formatting to expense amount. A few other improvements.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,5 @@
 class Expense < ActiveRecord::Base
+  validates :user_id, presence: true
+  validates :name, length: { in: 3..200 }
+  validates :amount, numericality: { greater_than_or_equal_to: 0 }
 end
