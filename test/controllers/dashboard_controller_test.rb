cd827a26ea03faad702154b93c24014c74ecc8ec
Alec Ferguson <alec.ferguson@gmail.com>
2014-09-21 10:46:05 -0700

    Added tests for DashboardController.

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  include SessionsHelper
+  setup do
+    @user = users(:one)
+  end
+
+  test 'Authed user should get :index' do
+    sign_in @user
+    get :index
+    assert_response :success
+  end
+
+  test 'Unauthed user cannot get :index' do
+    get :index
+    assert_response :redirect
+    assert_redirected_to({controller: :sessions, action: :login})
+  end
+
+  # TODO: Figure out a way to make the @tabs attribute accessible here. Jack the attr_reader
+  #       during test execution??
+  # test '@tabs is set', :skip do
+  #   @tab = tabs(:one)
+  #   sign_in @user
+  #   get :index
   # end
 end
